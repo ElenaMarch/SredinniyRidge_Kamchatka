eed7bf1eb98c55f28e16ee2dd3819cc7fdfd781e
ElenaMarch <39090571+ElenaMarch@users.noreply.github.com>
2018-08-17 22:34:07 +0200

    Update Kamchatka.js

--- js/Kamchatka.js
+++ js/Kamchatka.js
@@ -251,9 +251,10 @@ var sidebar = L.control.sidebar('sidebar', {closeButton: true}),
 			   <a href="http://www.ginras.ru/library/pdf/608_pevzner2015_holocene_volcanism_kamchatka.pdf"> ISBN 978-5-89118-682-8</a></p>\
 			   <h3 style="margin-bottom:-6px">Glaciers</h3>\
 			   <p class="lorem">Glaciers outlines were mapped using <a href="https://landsat.usgs.gov/">Landsat</a> Images (L7, L8 - August 2002, 2016) and orthophotoes taken from aircraft in 1949-1951. This dataset reflects glacial retreat in the Northern Kamchatka at the second part of 20th / beginning of 21th century and has been never published before.<br>&copy; Elena Marchenko, Yaroslav Muravyev</p>\
-			   <br><hr><h2 style="margin-bottom:-6px">Credits</h2>\
+			   <br><hr><h2>Credits</h2>\
 			   <p class="lorem">The webb-map is built using the awesome open-source JavaScript library <a href="https://leafletjs.com/">Leaflet</a>. Besides, it makes use of a set of Leaflet plugins (incl. <a href=”https://esri.github.io/esri-leaflet/”>ESRI for Leaflet</a>) and ArcGIS basemaps. The latter two are freely distributed for non-commercial use under Essentials Developer Plan (<a href=”https://github.com/esri/esri-leaflet#terms”>ESRI’s Terms of Use</a>).</p>\
-			   <p class="lorem" style="position: absolute; bottom:0">&copy; 2018 Elena Marchenko<br>PhD, Research fellow<br>\
+			   <br><hr>\
+			   <p class="lorem">&copy; 2018 Elena Marchenko<br>PhD, Research fellow<br>\
 			   <a href="http://www.kscnet.ru/ivs/">Institute of Volcanology and Seismology</a><br>Far Eastern Branch of Russian Academy<br> of Sciences</p>';
 
 map.addControl(sidebar);
