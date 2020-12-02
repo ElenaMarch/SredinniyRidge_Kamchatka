ae00376cc6fb23ed2c3b3cd1be3aad1939e73f5b
Marchenko <elena.marchenko@wsp.com>
2020-12-03 00:03:49 +0100

    Little fixes

--- js/general.js
+++ js/general.js
@@ -9,21 +9,21 @@ var map = L.map('map', {attributionControl: false}).setView([55.566,160.082], 7)
 var base = L.esri.basemapLayer('Topographic').addTo(map);
 
 // Coordinates and Scale
-function addControlPlaceholders(map) {
-    var corners = map._controlCorners,
-        l = 'leaflet-',
-        container = map._controlContainer;
+// function addControlPlaceholders(map) {
+//     var corners = map._controlCorners,
+//         l = 'leaflet-',
+//         container = map._controlContainer;
 
-    function createCorner(vSide, hSide) {
-        var className = l + vSide + ' ' + l + hSide;
-        corners[vSide + hSide] = L.DomUtil.create('div', className, container);
-    }
-    createCorner('bottom', 'center');
-}
-addControlPlaceholders(map);
+//     function createCorner(vSide, hSide) {
+//         var className = l + vSide + ' ' + l + hSide;
+//         corners[vSide + hSide] = L.DomUtil.create('div', className, container);
+//     }
+//     createCorner('bottom', 'center');
+// }
+// addControlPlaceholders(map);
 
 L.control.coordinates({
-	position:"bottomleft",
+	position:"bottomright",
 	decimals:3,
 	decimalSeperator:".",
 	labelTemplateLat:"N {y}&#176;&nbsp;&nbsp",
@@ -31,19 +31,19 @@ L.control.coordinates({
 	useLatLngOrder:true
 }).addTo(map);
 
-L.control.scale({imperial: false, position: 'bottomcenter'}).addTo(map);
+L.control.scale({imperial: false, position: 'bottomright'}).addTo(map);
 
 
-L.Control.Watermark = L.Control.extend({
-	onAdd: function(map) {
-		var img = L.DomUtil.create('img');
-		img.src = 'logo/IVIS.png';
-		img.style.width = '110px';
-		return img;},
-	onRemove: function(map) {} });
+// L.Control.Watermark = L.Control.extend({
+// 	onAdd: function(map) {
+// 		var img = L.DomUtil.create('img');
+// 		img.src = 'logo/IVIS.png';
+// 		img.style.width = '110px';
+// 		return img;},
+// 	onRemove: function(map) {} });
 
-L.control.watermark = function(opts) {return new L.Control.Watermark(opts); }
-L.control.watermark({ position: 'bottomright' }).addTo(map);
+// L.control.watermark = function(opts) {return new L.Control.Watermark(opts); }
+// L.control.watermark({ position: 'bottomright' }).addTo(map);
 
 // Button for references
 L.easyButton({id: 'reference',
--- js/navigation.js
+++ js/navigation.js
@@ -5,6 +5,14 @@
  */
 
 // BaseMap
+var circle_nv = {
+  radius: 4,
+  color: "#492622",
+  weight: 2,
+  opacity: 1,
+  fillOpacity: 0.5
+};
+
 var nv = L.map('mapSmall', {attributionControl: false, zoomControl: false}).setView([55.566,160.082], 4); // 7 ; 55.566,160.082
 var nv_base = L.esri.basemapLayer('Gray').addTo(nv),
   box = L.layerGroup().addTo(nv);
@@ -23,6 +31,9 @@ nv.on('click', function(e) {
 });
 
 addBox();
+// var kl_nv = L.circleMarker([56.056, 160.641], circle_nv).addTo(nv),
+//     sr_nv = L.circleMarker([57.686, 160.476], circle_nv).addTo(nv);
+
 nv.on('mouseover', function() {
   $("#mapSmall").tooltip({tooltipClass: "custom-tooltip-styling", track: true});
 });
