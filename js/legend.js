d58d2c1bb9d111c5a749b2b3360ba762d488af4c
ElenaMarch <eklmnk@gmail.com>
2023-03-28 09:55:32 -0700

    test

--- js/legend.js
+++ js/legend.js
@@ -86,7 +86,7 @@ div_glKluch.innerHTML += '<i class="hatching-cover" ></i><p>Ice-bound debris cov
 div_glKluch.innerHTML += '<i class="hatching-line" ></i><p>Fronts of creeping ice</p>';
 
 var div_hazard = L.DomUtil.create('div', 'legend');
-div_hazard.innerHTML += '<p style="line-height: 1.4"><b>Volcanic hazard zones (showing reoccurence of eruptions)</b></p>';
+div_hazard.innerHTML += '<p style="line-height: 1.4"><b>Volcanic hazard zones (i.e. reoccurence of eruptions)</b></p>';
 div_hazard.innerHTML += '<i style="height: 12px; width: 16px; border: 1.2px solid #633705;background-color:#D21704;"></i><p>1 time in 1-5 years</p>';
 div_hazard.innerHTML += '<i style="height: 12px; width: 16px; border: 1.2px solid #633705;background-color:#F05511;"></i><p>1 time in 10-50 years</p>';
 div_hazard.innerHTML += '<i style="height: 12px; width: 16px; border: 1.2px solid #633705;background-color:#FCA73C;"></i><p>1 time in 50-100 years</p>';
