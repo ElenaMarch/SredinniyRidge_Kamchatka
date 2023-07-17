82bd262b0e1169ad102ea1f1a6802c6d87ce46fc
EM-wsp <elena.marchenko.2@wsp.com>
2023-07-16 22:57:18 -0700

    test

--- js/kluch_group.js
+++ js/kluch_group.js
@@ -86,10 +86,10 @@ conesPobochLayer.bindPopup(function (layer) {
 	return L.Util.template(Cones_attr, Con(layer)); }, {'className':'popupCustom'});
 
 function conesStyle(feature) {
-	switch (feature.properties.Type_map) {
-      case 'large': return {"color": "#595959", 'shape': "triangle", 'radius': 2.5, "fillOpacity": 1, "weight":1, 'pane': 'Local3'};
-      case 'small': return {"color": "#595959", 'shape': "triangle", 'radius': 2.5, "fillOpacity": 1, "weight":1, 'pane': 'Local3'}; 
-      case 'shield': return {"color": "#595959", 'shape': "x", 'radius': 5, "weight":2, "fillOpacity": 0.75, 'pane': 'Local3'};    }
+	// switch (feature.properties.Type_map) {
+    //   case 'large': return {"color": "#595959", 'shape': "triangle", 'radius': 2.5, "fillOpacity": 1, "weight":1, 'pane': 'Local3'};
+    //   case 'small': return {"color": "#595959", 'shape': "triangle", 'radius': 2.5, "fillOpacity": 1, "weight":1, 'pane': 'Local3'}; 
+    //   case 'shield': return {"color": "#595959", 'shape': "x", 'radius': 5, "weight":2, "fillOpacity": 0.75, 'pane': 'Local3'};    }
 };
 
 var conesPobochPointsLayer = new L.GeoJSON.AJAX("json/cones_poboch_point.geojson",
